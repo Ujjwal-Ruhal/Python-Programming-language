afe0adbf1e7a256a87355015cbf6972952208c74
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-04-22 00:42:18 +0530

    My First Assignment in Python Lamguage

---
+++ Assignment-1/task_1.py
@@ -0,0 +1,15 @@
+'''
+write a python program to perform the basic mathematical operations 
+like ( add, subtract, multiply and divide) on two numbers.
+
+'''
+
+x = input("Enter the first number: ")
+x = int(x)
+y = input("Enter the second number: ")
+y = int(y)
+
+print("Addition: ", x+y)
+print("Subtraction: ", x-y)
+print("Multiplication: ", x*y)
+print("Division: ", x/y)
---
+++ Assignment-1/task_2.py
@@ -0,0 +1,8 @@
+'''
+write a python program to print a personalized greeting massage using the full name.
+
+'''
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello, " + first_name + " " + last_name + "!"+" Welcome to My First GitHub Repository.")
